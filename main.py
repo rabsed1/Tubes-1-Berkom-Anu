af839c147f80ba777980746e3584139fab91131c
Fachry Azriel <frielfajdwani@gmail.com>
2025-10-27 12:49:04 +0700

    Nganu (liat desc)
    Ngerjain main.py, nambah kolom baru di kartu.csv,
    and nambahin trailling space di akhir file2 yang
    ada.

--- main.py
+++ main.py
@@ -7,4 +7,34 @@ if __name__ != "__main__":
 import stasiun
 import tarif
 import helper
+import database
 
+helper.bersihkan_layar()
+kartu_penumpang = None
+
+while True:
+    id_penumpang = input("Silakan masukkan ID kartu Anda: ")
+    kartu_penumpang = helper.cari(database.daftar_kartu, "ID", id_penumpang)
+
+    if len(id_penumpang) != 10 or id_penumpang != id_penumpang.upper():
+        print("ID yang dimasukkan tidak valid!")
+        continue
+
+    if not kartu_penumpang:
+        print("\nMaaf, kartu Tidak Terdaftar!")
+        helper.tunggu(0.8)
+        helper.bersihkan_layar()
+        continue
+    
+    break
+
+helper.bersihkan_layar()
+print(
+    f"ID: {kartu_penumpang["ID"]}\n",
+    f"Nama: {kartu_penumpang["Nama"]}\n",
+    f"Saldo: Rp{int(kartu_penumpang["Saldo"]):,}".replace(",","."),
+    sep=""
+)
+
+print("\n")
+    
